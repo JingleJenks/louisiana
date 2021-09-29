a0b04786a2da9dfea77a125bc633731cb47856b5
JingleJenks <sylv2439@stud.kea.dk>
2021-09-29 11:13:26 +0200

    card2-9 try added

--- memory.js
+++ memory.js
@@ -13,19 +13,71 @@ let Front = document.querySelector(".front_face")
 
 function loadgame() {
     console.log("loadgame");
-    One.addEventListener("click", flipcard);
-    Two.addEventListener("click", flipcard);
-    Three.addEventListener("click", flipcard);
-    Four.addEventListener("click", flipcard);
-    Five.addEventListener("click", flipcard);
-    Six.addEventListener("click", flipcard);
-    Seven.addEventListener("click", flipcard);
-    Eight.addEventListener("click", flipcard);
-    Nine.addEventListener("click", flipcard);
+    One.addEventListener("click", flipcardOne);
+    Two.addEventListener("click", flipcardTwo);
+    Three.addEventListener("click", flipcardThree);
+    Four.addEventListener("click", flipcardFour);
+    Five.addEventListener("click", flipcardFive);
+    Six.addEventListener("click", flipcardSix);
+    Seven.addEventListener("click", flipcardSeven);
+    Eight.addEventListener("click", flipcardEight);
+    Nine.addEventListener("click", flipcardNine);
 
 }
 
 function flipcardOne() {
     console.log("card1 clicked");
-    One.classList.remove(".front_face");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+}
+
+function flipcardTwo() {
+    console.log("card2 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+
+}
+
+function flipcardThree() {
+    console.log("card3 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+
+}
+
+function flipcardFour() {
+    console.log("card4 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+
+}
+
+function flipcardFive() {
+    console.log("card5 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+}
+
+function flipcardSix() {
+    console.log("card6 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+}
+
+function flipcardSeven() {
+    console.log("card7 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+}
+
+function flipcardEight() {
+    console.log("card8 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
+}
+
+function flipcardNine() {
+    console.log("card9 clicked");
+    // One.classList.add(".invisible");
+    One.classList.add(".card_flip");
 }
