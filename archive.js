2857e8727790e1ef877d7bc95b24fcb73325fce8
JingleJenks <sylv2439@stud.kea.dk>
2021-09-29 21:40:44 +0200

    archivve added

---
+++ archive.js
@@ -0,0 +1,13 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+// const id = urlParams.get("id");
+
+const url = "https://verdi-b148.restdb.io/rest/verdi ";
+
+console.log(url);
+
+const key = {
+    headers: {
+        "x-apikey": "9e04167745415ced787fc395095e6739124df",
+    },
+};
