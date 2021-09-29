dd4e7a576bf09d0904ef6329b320b48692820368
JingleJenks <sylv2439@stud.kea.dk>
2021-09-29 09:56:17 +0200

    js so far

--- memory.js
+++ memory.js
@@ -1,1 +1,23 @@
-window.addEventListener("load", Welcome);
+window.addEventListener("load", loadgame);
+let One = document.querySelector(".one");
+let Two = document.querySelector(".two");
+let Three = document.querySelector(".three");
+let Four = document.querySelector(".four");
+let Five = document.querySelector(".five");
+let Six = document.querySelector(".six");
+let Seven = document.querySelector(".seven");
+let Eight = document.querySelector(".eight");
+let Nine = document.querySelector(".nine");
+let Front = document.querySelector(".front_face")
+
+
+function loadgame() {
+    console.log("loadgame");
+    Front.addEventListener("click", flipcard);
+
+}
+
+function flipcard() {
+    Front.classList.toggle(". card_flip");
+
+}
