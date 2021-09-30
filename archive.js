86993c4a99d4c436956ac930390f74d4105a44eb
JingleJenks <sylv2439@stud.kea.dk>
2021-09-30 16:43:23 +0200

    game update

--- archive.js
+++ archive.js
@@ -15,6 +15,86 @@ fetch(url, key)
         return res.json();
     })
     .then(function (data) {
-        // showArt(data);
-        console.log(data);
+        handleArchive(data);
     })
+
+// function handleData(data) {
+//         console.log(data);
+//         data.forEach(showData);
+//     }
+
+
+
+function handleArchive(data) {
+    console.log(data);
+    data.forEach(showArt);
+}
+
+
+function showArt(obj) {
+    console.log(obj);
+    console.log("yo");
+    const template1 = document.querySelector(".flip-card1").content;
+    const clone = template1.cloneNode(true);
+    clone.querySelector(".img").textContent = obj.image;
+    // clone.querySelector("img").src = obj.image;
+    const mainEl = document.querySelector("main");
+    // const mainEl = document.querySelector("body");
+    mainEl.appendChild(clone);
+}
+
+
+
+
+
+// function showArt(obj) {
+//     console.log(obj);
+//     console.log("yo");
+//     const template = document.querySelector(".speciestemp").content;
+//     const clone = template.cloneNode(true);
+//     clone.querySelector(".speciesh1").textContent = obj.species;
+//     // clone.querySelector("img").src = obj.image;
+//     const mainEl = document.querySelector("main");
+//     // const mainEl = document.querySelector("body");
+//     mainEl.appendChild(clone);
+// }
+
+
+
+
+
+
+// function showimg2(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
+
+// function showimg3(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
+
+// function showimg4(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
+
+// function showimg6(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
+
+// function showimg7(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
+
+// function showimg8(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
+
+// function showimg9(obj) {
+//     console.log(obj);
+//     One.("img").src = obj.image;
+// }
