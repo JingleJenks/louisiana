71ebdbd0b488969d127e3399c73002f8c4fd8a8e
JingleJenks <sylv2439@stud.kea.dk>
2021-09-30 13:23:48 +0200

    js memory working!!!

--- archive.js
+++ archive.js
@@ -1,12 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
 // const img = urlParams.get ()
-const url = "https://verdi-b148.restdb.io/rest/verdi/";
+const url = "https://verdi-b148.restdb.io/rest/verdi";
 
 console.log(url);
 
 const key = {
     headers: {
-        "x-apikey": "9e04167745415ced787fc395095e6739124df",
+        "x-apikey": "61556b32dfa7346e2f9691a1",
     },
 };
 
--- memory.js
+++ memory.js
@@ -4,14 +4,14 @@ let One = document.querySelector(".flip-card1");
 let Two = document.querySelector(".flip-card2");
 let Three = document.querySelector(".flip-card3");
 let Four = document.querySelector(".flip-card4");
-let Five = document.querySelector(".flip-card5");
+// let Five = document.querySelector(".flip-card5");
 let Six = document.querySelector(".flip-card6");
 let Seven = document.querySelector(".flip-card7");
 let Eight = document.querySelector(".flip-card8");
 let Nine = document.querySelector(".flip-card9");
 let Front = document.querySelector(".flip-card-front");
 let Inner = document.querySelector(".flip-card-inner");
-
+let Body = document.querySelector("body");
 
 function loadgame() {
     console.log("loadgame");
@@ -19,7 +19,7 @@ function loadgame() {
     Two.addEventListener("click", flipcardTwo);
     Three.addEventListener("click", flipcardThree);
     Four.addEventListener("click", flipcardFour);
-    Five.addEventListener("click", flipcardFive);
+    // Five.addEventListener("click", flipcardFive);
     Six.addEventListener("click", flipcardSix);
     Seven.addEventListener("click", flipcardSeven);
     Eight.addEventListener("click", flipcardEight);
@@ -27,154 +27,279 @@ function loadgame() {
 
 }
 
+function checkForMisMatch() {
+
+    const active = document.querySelectorAll(".flip_js");
+
+    if (active.length == 2) {
+
+        score = 0;
+
+        setTimeout(() => {
+
+            active.forEach((elem) => elem.classList.remove("flip_js"));
+
+        }, 1000);
+
+    }
+
+}
+
 function flipcardOne() {
-    console.log("card1 clicked");
     One.classList.add("flip_js");
     score += 1;
     console.log(score);
-    console.log("card 1a, +1");
+    console.log("card 1/1a, +1");
     if (score == 2) {
         paintingbox();
+    } else {
+
+        checkForMisMatch();
+
     }
 }
 
+
+// else {
+
+//     const active = document.querySelectorAll(".flip_js");
+
+//     if(active.length==2){
+
+//     active.forEach(elem=>elem.classList.remove("flip_js"))
+
+//     }
+
+//     }
+
 function flipcardTwo() {
-    console.log("card2 clicked");
     Two.classList.add("flip_js");
 
     score += 3;
     console.log(score);
 
-    console.log("card 2a, +3");
+    console.log("card 2/2a, +3");
     if (score == 6) {
         photobox();
+    } else {
+
+        checkForMisMatch();
+
     }
 
 }
 
 function flipcardThree() {
-    console.log("card3 clicked");
     Three.classList.add("flip_js");
 
     score += 7;
     console.log(score);
 
-    console.log("card 3a, +7");
+    console.log("card 3/3a, +7");
     if (score == 14) {
         abstractbox();
+    } else {
+
+        checkForMisMatch();
+
     }
 
 }
 
 function flipcardFour() {
-    console.log("card4 clicked");
     Four.classList.add("flip_js");
 
     score += 9;
     console.log(score);
 
-    console.log("card 4a, +9");
+    console.log("card 4/4a, +9");
     if (score == 18) {
         popbox();
+    } else {
+
+        checkForMisMatch();
+
     }
 
 }
 
-function flipcardFive() {
-    console.log("card5 clicked");
-    Five.classList.add("flip_js");
+// // function flipcardFive() {
+// Five.classList.add("flip_js");
 
-    score += 0;
-    console.log(score);
-}
+// score += 0;
+// console.log(score);
+// console.log("card5 clicked");
+
+// }
 
 function flipcardSix() {
-    console.log("card6 clicked");
     Six.classList.add("flip_js");
 
     score += 7;
     console.log(score);
 
-    console.log("card 3a, +7");
+    console.log("card 6/3a, +7");
     if (score == 14) {
         abstractbox();
+    } else {
+
+        checkForMisMatch();
+
     }
 }
 
 function flipcardSeven() {
-    console.log("card7 clicked");
     Seven.classList.add("flip_js");
 
     score += 3;
     console.log(score);
 
-    console.log("card 3b, +7");
+    console.log("card 7/3b, +7");
     if (score == 6) {
         photobox();
+    } else {
+
+        checkForMisMatch();
+
     }
 
 }
 
 function flipcardEight() {
-    console.log("card8 clicked");
     Eight.classList.add("flip_js");
-
     score += 1;
     console.log(score);
 
-    console.log("card 1b, +3");
+    console.log("card8/1b, +1");
     if (score == 2) {
         paintingbox();
+    } else {
+
+        checkForMisMatch();
+
     }
 
 }
 
 function flipcardNine() {
-    console.log("card9 clicked");
     Nine.classList.add("flip_js");
 
     score += 9;
     console.log(score);
-    console.log("card 4b, +9");
+    console.log("card 9/4b, +9");
     if (score == 18) {
         popbox();
+    } else {
+
+        checkForMisMatch();
+
     }
 }
 
-// function nextpair() {
-//     console.log("you win!");
-//     score = 0;
-//     if (score == 36) {
-//         game_end();
-//     }
-//     console.log(score);
-
-// }
-
 function game_end() {
-    console.log("game over!");
-    console.log(score);
+    if (popbox_shown && paintingbox_shown && abstractbox_shown && photobox_shown) {
+        console.log("game over!");
+        console.log(score);
+        document.querySelector(".box_pop").classList.add("invisible");
+        document.querySelector(".box_abstract").classList.add("invisible");
+        document.querySelector(".box_photo").classList.add("invisible");
+        document.querySelector(".box_painting").classList.add("invisible");
+
+        const cards = document.querySelectorAll(".memory_grid > *")
+        cards.forEach((elem, index) => {
+
+            setTimeout(() => {
+
+                elem.classList.add("flip_js")
+
+            }, index * 500 + 500)
+
+        })
+        setTimeout(() => {
+            console.log("game over box");
+            document.querySelector(".box_end").classList.remove("invisible");
+
+        }, 5000)
+    }
+
 
 }
+
+let popbox_shown = false;
+let paintingbox_shown = false;
+let abstractbox_shown = false;
+let photobox_shown = false;
+
+
 
 function popbox() {
     console.log("popbox info");
     document.querySelector(".box_pop").classList.remove("invisible");
+    setTimeout(flipback4, 3000)
+    // document.querySelector(".box_pop").addEventListener("click", flipback4);
+    popbox_shown = true;
+    game_end();
 }
 
 function paintingbox() {
     console.log("paintingbox info");
     document.querySelector(".box_painting").classList.remove("invisible");
-    One.classList.add("flip_js");
+    setTimeout(flipback1, 3000)
+    // document.querySelector(".box_painting").addEventListener("click", flipback1);
+    paintingbox_shown = true;
+    game_end();
 }
 
 function abstractbox() {
     console.log("abstractbox info");
     document.querySelector(".box_abstract").classList.remove("invisible");
+    setTimeout(flipback3, 3000)
+    // document.querySelector(".box_abstract").addEventListener("click", flipback3);
+    abstractbox_shown = true;
+    game_end();
 }
 
 function photobox() {
     console.log("photobox info");
     document.querySelector(".box_photo").classList.remove("invisible");
+    setTimeout(flipback2, 3000)
+    // document.querySelector(".box_photo").addEventListener("click", flipback2);
+    photobox_shown = true;
+    game_end();
+}
 
+function flipback1() {
+    console.log("flipback1");
+    One.classList.remove("flip_js");
+    Eight.classList.remove("flip_js");
+    document.querySelector(".box_painting").classList.add("invisible");
+    score = 0;
+    // loadgame();
+}
+
+function flipback2() {
+    console.log("flipback2");
+    Two.classList.remove("flip_js");
+    Seven.classList.remove("flip_js");
+    document.querySelector(".box_photo").classList.add("invisible");
+    score = 0;
+    // loadgame();
+}
+
+function flipback3() {
+    console.log("flipback3");
+    Three.classList.remove("flip_js");
+    Six.classList.remove("flip_js");
+    document.querySelector(".box_abstract").classList.add("invisible");
+    score = 0;
+    // loadgame();
+}
+
+function flipback4() {
+    console.log("flipback4");
+    Four.classList.remove("flip_js");
+    Nine.classList.remove("flip_js");
+    document.querySelector(".box_pop").classList.add("invisible");
+    score = 0;
+    // loadgame();
 }
